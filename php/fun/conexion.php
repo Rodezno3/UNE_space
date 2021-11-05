9666c34e7133194d3932c650298fcb77b9e65239
David Rodezno <idavid.21gr@gmail.com>
2021-11-04 22:24:10 -0600

    Update conexion.php

--- php/fun/conexion.php
+++ php/fun/conexion.php
@@ -1,7 +1,7 @@
 <?php
-    $db_host = "localhost";
-    $db_nombre = "xoxoctic_beta";
-    $db_usuario = "root";
+    $db_host = "";
+    $db_nombre = "";
+    $db_usuario = "";
     $db_pass = "";
 
     $conexionb = mysqli_connect($db_host,$db_usuario,$db_pass);
