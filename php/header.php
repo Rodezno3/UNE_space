366a283d688c2b60ec0ff297023ec8c857c9d3cf
David Rodezno <idavid.21gr@gmail.com>
2021-11-04 18:59:38 -0600

    Add files via upload

---
+++ php/header.php
@@ -0,0 +1,12 @@
+    <header>
+        <ul>
+            <a href=""><li><img src="img/logo.png" alt="logo" style="padding:5px;"></li></a>
+            <a href="inicio.php"><li>Inicio</li></a>
+            <a href="nueva_orden.php"><li>Nueva Orden</li></a>
+            <a href="menu.php"><li>Menú</li></a>
+            <a href="frecuente.php"><li>Frecuentes</li></a>
+            <a href="count.php"><li>Registro</li></a>
+            <a href="registro.php"><li>Base de Datos</li></a>
+            <a href="php/salir.php"><li>Salir</li></a>
+        </ul>
+    </header>
